380d82b437d8e225777793b0acff0dcf2f93fa54
Farrelio <imcominfordoom@gmail.com>
2025-09-09 02:18:36 +0700

    refcator: adjust the laravel view root

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    @viteReactRefresh
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite('resources/js/app.tsx')
+    @production
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+    @else
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
+    @endproduction
     <link rel="icon" type="image/png" href="/assets/compsphere.png">
     @inertiaHead
 </head>
