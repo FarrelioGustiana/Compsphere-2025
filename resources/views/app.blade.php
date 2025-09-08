47ec9bb0e304de909db2b29445e0a367f9fedd29
Farrelio <imcominfordoom@gmail.com>
2025-09-09 05:48:34 +0700

    refactor: asjust laravel view root

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,16 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    @if(app()->environment('local'))
-        @viteReactRefresh
-    @endif
+    @viteReactRefresh
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @vite(['resources/js/app.tsx'])
+    @vite('resources/js/app.tsx')
     <link rel="icon" type="image/png" href="/assets/compsphere.png">
     @inertiaHead
 </head>
-{{-- Root --}}
 <body style="background-color: #101828;">
+    @routes
+    @inertia
+</body>
+</html>
+
     @routes
     @inertia
 </body>
