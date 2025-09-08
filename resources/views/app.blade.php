46278705041da56e2ca3dec1819914f465778b9c
Farrelio <imcominfordoom@gmail.com>
2025-09-09 03:13:52 +0700

    fix: test the solution for the error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,15 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    @viteReactRefresh
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    @production
-        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
-    @else
-        @viteReactRefresh
-        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
-    @endproduction
-    <link rel="icon" type="image/png" href="{{ asset('assets/compsphere.png') }}">
+    @vite('resources/js/app.tsx')
+    <link rel="icon" type="image/png" href="/assets/compsphere.png">
     @inertiaHead
 </head>
 <body style="background-color: #101828;">
